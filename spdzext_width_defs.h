f6e7fde6e2779a375db6aa18a255cc0a88047df2
Ran Proshan <ran.proshan@gmail.com>
2018-10-30 01:57:40 +0200

    Added width defs

--- spdzext_width_defs.h
+++ spdzext_width_defs.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
-#define			GFP_VECTOR		1
+#define			GFP_VECTOR		100
 #define			GFP_LIMBS		GFP_VECTOR*2
+#define			SHR_LIMBS		GFP_LIMBS*2
 #define			GFP_BYTES		GFP_LIMBS*sizeof(mp_limb_t)
+#define			SHR_BYTES		SHR_LIMBS*sizeof(mp_limb_t)
