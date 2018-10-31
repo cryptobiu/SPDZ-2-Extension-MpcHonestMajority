95a4bb79c934bb8b6a8199021ca5731193e857b8
Ran Proshan <ran.proshan@gmail.com>
2018-10-31 07:53:37 +0200

    GFP vector width defs

--- spdzext_width_defs.h
+++ spdzext_width_defs.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#define			GFP_VECTOR		100
+#define			GFP_VECTOR		1
 #define			GFP_LIMBS		GFP_VECTOR*2
 #define			SHR_LIMBS		GFP_LIMBS*2
 #define			GFP_BYTES		GFP_LIMBS*sizeof(mp_limb_t)
